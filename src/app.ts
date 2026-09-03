20f15906b962c78688c8af6b76d93903c5135ea3
namgel <namgeltamang14@gmail.com>
2026-09-03 14:29:49 +0545

    Configure helmet

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import express, { type Request, type Response } from "express";
 import { env } from "./config/env.js";
 import { errorHandler } from "./errors/error-handler.js";
 import { notFound } from "./errors/not-found.js";
+import helmet from "helmet";
 
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 app.use(cors({ origin: env.FRONTEND_URL, credentials: true }));
 
