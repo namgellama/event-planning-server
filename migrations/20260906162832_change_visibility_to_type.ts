cb0302b2f47da9a65079b378807d7af87ac8b7fc
namgel <namgeltamang14@gmail.com>
2026-09-10 15:06:32 +0545

    Fix indexes

--- migrations/20260906162832_change_visibility_to_type.ts
+++ migrations/20260906162832_change_visibility_to_type.ts
@@ -3,6 +3,12 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.alterTable("events", (table) => {
         table.renameColumn("visibility", "type");
+        table.dropIndex("visibility");
+        table.dropIndex("date");
+    });
+
+    await knex.schema.alterTable("tags", (table) => {
+        table.index("user_id");
     });
 
     await knex.raw(`
@@ -17,7 +23,13 @@ export async function down(knex: Knex): Promise<void> {
             RENAME TO event_visibility
     `);
 
+    await knex.schema.alterTable("tags", (table) => {
+        table.dropIndex("user_id");
+    });
+
     return knex.schema.alterTable("events", (table) => {
         table.renameColumn("type", "visibility");
+        table.index("visibility");
+        table.index("date");
     });
 }
